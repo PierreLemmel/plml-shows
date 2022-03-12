989f7c3d52c1d7819140a467c04ab619890d5dfd
Pierre Lemmel <pierre.lemmel@gmail.com>
2022-03-13 00:17:06 +0100

    Fixture groups

---
+++ Assets/Core/Runtime/Dmx/Fixtures/DmxFixtureGroup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plml.Dmx
+{
+    public class DmxFixtureGroup : MonoBehaviour
+    {
+        public DmxFixture[] fixtures;
+    }
+}
