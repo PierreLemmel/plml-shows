7bce4f92570ca6287fdb3ccf576efeea4d068e2d
Pierre Lemmel <pierre.lemmel@gmail.com>
2022-04-04 18:32:40 +0200

    WIP

---
+++ Assets/Shows/Rng/Runtime/Serialization/RngSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Plml.Rng
+{
+    public class RngSerializer : MonoBehaviour
+    {
+        public RngSerializerSettings settings;
+
+        public void SerializeShow(RngShow show)
+        {
+
+        }
+
+
+        private class RngShowDataModel
+        {
+            public RngShowSettingsDataModel Settings { get; set; }
+            public IEnumerable<RngSceneDataModel> Scenes { get; set; }
+        }
+
+        private class RngShowSettingsDataModel
+        {
+        }
+
+
+        private class RngSceneDataModel
+        {
+            public TimeWindow TimeWindow { get; set; }
+
+            public AudioDataModel Audio { get; set; }
+        }
+
+        private class AudioDataModel
+        {
+            public string ClipName { get; set; }
+            public float Volume { get; set; }
+
+            public TimeWindow TimeWindow { get; set; }
+        }
+    }
+}
---
+++ Assets/Shows/Rng/Runtime/Serialization/RngSerializerSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Plml.Rng
+{
+    [Serializable]
+    public class RngSerializerSettings
+    {
+        public string path = "../Shows/Rng";
+
+        public string prefix = "Rng";
+    }
+}
