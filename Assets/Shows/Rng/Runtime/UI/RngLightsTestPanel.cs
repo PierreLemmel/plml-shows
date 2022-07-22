7cbd2e5627e7429b2607bda8eb5cd3e5224cba52
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2022-07-22 18:49:20 +0200

    WIP

---
+++ Assets/Shows/Rng/Runtime/UI/RngLightsTestPanel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Plml.Rng.UI
+{
+    public class RngLightsTestPanel : MonoBehaviour
+    {
+        public TMP_Dropdown scenesDropdown;
+
+
+    } 
+}
