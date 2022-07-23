4735c1152e3b07b2d4e240860c6d8632367128da
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2022-07-23 20:26:31 +0200

    WIP

--- Assets/Shows/Rng/Runtime/UI/RngLightsTestPanel.cs
+++ Assets/Shows/Rng/Runtime/UI/RngLightsTestPanel.cs
@@ -1,7 +1,11 @@
+using Plml.Dmx;
+using Plml.Rng.Dmx;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Plml.Rng.UI
 {
@@ -9,6 +13,80 @@ namespace Plml.Rng.UI
     {
         public TMP_Dropdown scenesDropdown;
 
+        public Slider durationSlider;
+        public TMP_Text durationLabel;
 
-    } 
+        public Button playBtn;
+        public Button stopBtn;
+
+        private DmxTrackProvider[] providers;
+        private DmxTrackControler dmxControler;
+
+        private int sceneIndex = 0;
+
+        private void Awake()
+        {
+            durationSlider.onValueChanged.AddListener(UpdateDurationLabel);
+
+            providers = FindObjectsOfType<DmxTrackProvider>();
+            dmxControler = FindObjectOfType<DmxTrackControler>();
+
+            scenesDropdown.ClearOptions();
+            scenesDropdown.options.AddRange(providers.Select(pvd => new TMP_Dropdown.OptionData(pvd.name)));
+
+            scenesDropdown.onValueChanged.AddListener(SetSceneIndex);
+
+            playBtn.onClick.AddListener(PlayScene);
+            stopBtn.onClick.AddListener(StopScene);
+
+            stopBtn.interactable = false;
+        }
+
+        public void UpdateDurationLabel(float duration) => durationLabel.text = $"{duration:F2} s";
+
+        public void SetSceneIndex(int sceneIndex)
+        {
+            this.sceneIndex = sceneIndex;
+        }
+
+        private Guid trackId = Guid.Empty;
+        private DmxTrack currentTrack = null;
+
+        private void StopCurrentScene()
+        {
+            if (currentTrack != null)
+            {
+                dmxControler.RemoveTrack(trackId);
+                Destroy(currentTrack.gameObject);
+                currentTrack = null;
+            }
+        }
+
+        private void UpdateButtons(bool currentlyPlaying)
+        {
+            scenesDropdown.interactable = true;
+            playBtn.interactable = true;
+            stopBtn.interactable = currentlyPlaying;
+        }
+
+        public void PlayScene()
+        {
+            StopCurrentScene();
+
+            UpdateButtons(true);
+
+            DmxTrackProvider provider = providers[sceneIndex];
+            currentTrack = provider.GetElement();
+            currentTrack.AttachTo(this);
+
+            dmxControler.AddTrack(currentTrack, out trackId);
+        }
+
+        public void StopScene()
+        {
+            StopCurrentScene();
+
+            UpdateButtons(false);
+        }
+    }
 }
