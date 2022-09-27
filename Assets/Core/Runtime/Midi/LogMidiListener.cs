14265ddda0993cb8209411ac380d03cb07d40844
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2022-09-28 00:24:16 +0200

    WIP

--- Assets/Core/Runtime/Midi/LogMidiListener.cs
+++ Assets/Core/Runtime/Midi/LogMidiListener.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 namespace Plml.Midi
 {
-    public class LogMidiListener : MidiInputListener
+    public class LogMidiListener : MidiInputListenerBase
     {
-        public override void OnNoteOn(byte keyNumber, byte velocity) => Debug.Log($"On note on: {keyNumber} {velocity}");
-        public override void OnNoteOff(byte keyNumber, byte velocity) => Debug.Log($"On note off: {keyNumber} {velocity}");
+        public override void OnNoteOn(MidiNote note, byte velocity) => Debug.Log($"On note on: {note} {velocity}");
+        public override void OnNoteOff(MidiNote note, byte velocity) => Debug.Log($"On note off: {note} {velocity}");
         public override void OnPolyphonicKeyPressure(int keyNumber, byte pressure) => Debug.Log($"On polyphonic key pressure: {keyNumber} {pressure}");
         public override void OnControlChanged(int controllerNumber, byte value) => Debug.Log($"On control changed: {controllerNumber} {value}");
         public override void OnProgramChanged(int program) => Debug.Log($"On program changed: {program}");
--- Assets/Core/Runtime/Midi/MidiInputListener.cs
+++ Assets/Core/Runtime/Midi/MidiInputListener.cs
@@ -2,46 +2,227 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Plml.Midi
 {
-    public abstract class MidiInputListener : MonoBehaviour
+    public sealed class MidiInputListener : MidiInputListenerBase
     {
-        public void OnShortMessage(byte status, byte data1, byte data2)
+        #region Actions
+        [SerializeField, EditTimeOnly]
+        private NoteOnData[] notesOn;
+        [SerializeField, EditTimeOnly]
+        private NoteOffData[] notesOff;
+        [SerializeField, EditTimeOnly]
+        private PolyphonicKeyPressureData[] polyphonicKeysPressure;
+        [SerializeField, EditTimeOnly]
+        private ControlChangedData[] controlsChanged;
+        [SerializeField, EditTimeOnly]
+        private ProgramChangedData[] programsChanged;
+
+
+        [SerializeField]
+        private UnityEvent<MidiNote, byte> onNoteOn;
+
+        [SerializeField]
+        private UnityEvent<MidiNote, byte> onNoteOff;
+
+        [SerializeField]
+        private UnityEvent<int, byte> onPolyphonicKeyPressure;
+
+        [SerializeField]
+        private UnityEvent<int, byte> onControlChanged;
+
+        [SerializeField]
+        private UnityEvent<int> onProgramChanged;
+
+        [SerializeField]
+        private UnityEvent<byte> onChannelPressure;
+
+        [SerializeField]
+        private UnityEvent<int> onPitchBend;
+
+
+        private IDictionary<MidiNote, UnityEvent<byte>> notesOnDic;
+        private IDictionary<MidiNote, UnityEvent<byte>> notesOffDic;
+        private IDictionary<int, UnityEvent<byte>> pkpDic;
+        private IDictionary<int, UnityEvent<byte>> ccDic;
+        private IDictionary<int, UnityEvent> pcDic;
+        #endregion
+
+        #region Lifecycle
+        private void Awake()
         {
-            switch (status / 0x10)
+            notesOnDic = notesOn.ToDictionary(
+                no => no.note,
+                no => no.action
+            );
+
+            notesOffDic = notesOff.ToDictionary(
+                no => no.note,
+                no => no.action
+            );
+
+            pkpDic = polyphonicKeysPressure.ToDictionary(
+                pkp => pkp.key,
+                pkp => pkp.action
+            );
+
+            ccDic = controlsChanged.ToDictionary(
+                cc => cc.controller,
+                cc => cc.action
+            );
+
+            pcDic = programsChanged.ToDictionary(
+                pc => pc.program,
+                pc => pc.action
+            );
+        }
+        #endregion
+
+        #region Add/Remove Listeners
+        public void AddNoteOnListener(MidiNote note, UnityAction<byte> action)
+        {
+            if (!notesOnDic.TryGetValue(note, out UnityEvent<byte> ue))
             {
-                case 0x8:
-                    OnNoteOff(data1, data2);
-                    break;
-                case 0x9:
-                    OnNoteOn(data1, data2);
-                    break;
-                case 0xa:
-                    OnPolyphonicKeyPressure(data1, data2);
-                    break;
-                case 0xb:
-                    OnControlChanged(data1, data2);
-                    break;
-                case 0xc:
-                    OnProgramChanged(data1);
-                    break;
-                case 0xd:
-                    OnChannelPressure(data1);
-                    break;
-                case 0xe:
-                    int pitch = 0x100 * data1 + data2;
-                    OnPitchBend(pitch);
-                    break;
+                ue = new();
+                notesOnDic.Add(note, ue);
             }
+
+            ue.AddListener(action);
         }
 
-        public virtual void OnNoteOn(byte keyNumber, byte velocity) { }
-        public virtual void OnNoteOff(byte keyNumber, byte velocity) { }
-        public virtual void OnPolyphonicKeyPressure(int keyNumber, byte pressure) { }
-        public virtual void OnControlChanged(int controllerNumber, byte value) { }
-        public virtual void OnProgramChanged(int program) { }
-        public virtual void OnChannelPressure(byte pressure) { }
-        public virtual void OnPitchBend(int pitch) { }
+        public void AddNoteOffListener(MidiNote note, UnityAction<byte> action)
+        {
+            if (!notesOffDic.TryGetValue(note, out UnityEvent<byte> ue))
+            {
+                ue = new();
+                notesOffDic.Add(note, ue);
+            }
+
+            ue.AddListener(action);
+        }
+
+        public void AddPolyphonicKeyPressureListener(int keyNumber, UnityAction<byte> action)
+        {
+            if (!pkpDic.TryGetValue(keyNumber, out UnityEvent<byte> ue))
+            {
+                ue = new();
+                pkpDic.Add(keyNumber, ue);
+            }
+
+            ue.AddListener(action);
+        }
+
+        public void AddControlChangedListener(int controllerNumber, UnityAction<byte> action)
+        {
+            if (!ccDic.TryGetValue(controllerNumber, out UnityEvent<byte> ue))
+            {
+                ue = new();
+                ccDic.Add(controllerNumber, ue);
+            }
+
+            ue.AddListener(action);
+        }
+
+        public void AddProgramChangedListener(int program, UnityAction action)
+        {
+            if (!pcDic.TryGetValue(program, out UnityEvent ue))
+            {
+                ue = new();
+                pcDic.Add(program, ue);
+            }
+
+            ue.AddListener(action);
+        }
+
+        public void RemoveNoteOnListener(MidiNote note, UnityAction<byte> action) => notesOnDic[note].RemoveListener(action);
+        public void RemoveNoteOffListener(MidiNote note, UnityAction<byte> action) => notesOffDic[note].RemoveListener(action);
+        public void RemovePolyphonicKeyPressureListener(int keyNumber, UnityAction<byte> action) => pkpDic[keyNumber].RemoveListener(action);
+        public void RemoveControlChangedListener(int controllerNumber, UnityAction<byte> action) => ccDic[controllerNumber].RemoveListener(action);
+        public void RemoveProgramChangedListener(int program, UnityAction action) => pcDic[program].RemoveListener(action);
+        #endregion
+
+        #region Overrides
+        public override void OnNoteOn(MidiNote note, byte velocity)
+        {
+            onNoteOn?.Invoke(note, velocity);
+
+            if (notesOnDic != null && notesOnDic.TryGetValue(note, out var handler))
+                handler.Invoke(velocity);
+        }
+
+        public override void OnNoteOff(MidiNote note, byte velocity)
+        {
+            onNoteOff?.Invoke(note, velocity);
+
+            if (notesOnDic != null && notesOffDic.TryGetValue(note, out var handler))
+                handler.Invoke(velocity);
+        }
+
+        public override void OnPolyphonicKeyPressure(int keyNumber, byte pressure)
+        {
+            onPolyphonicKeyPressure?.Invoke(keyNumber, pressure);
+
+            if (notesOnDic != null && pkpDic.TryGetValue(keyNumber, out var handler))
+                handler.Invoke(pressure);
+        }
+
+        public override void OnControlChanged(int controllerNumber, byte value)
+        {
+            onControlChanged?.Invoke(controllerNumber, value);
+
+            if (ccDic != null && ccDic.TryGetValue(controllerNumber, out var handler))
+                handler?.Invoke(value);
+        }
+
+        public override void OnProgramChanged(int program)
+        {
+            onProgramChanged?.Invoke(program);
+
+            if (pcDic != null && pcDic.TryGetValue(program, out var handler))
+                handler?.Invoke();
+        }
+
+        public override void OnChannelPressure(byte pressure) => onChannelPressure?.Invoke(pressure);
+        public override void OnPitchBend(int pitch) => onPitchBend?.Invoke(pitch);
+        #endregion
+
+        #region Structs
+        [Serializable]
+        public struct NoteOnData
+        {
+            public MidiNote note;
+            public UnityEvent<byte> action;
+        }
+
+        [Serializable]
+        public struct NoteOffData
+        {
+            public MidiNote note;
+            public UnityEvent<byte> action;
+        }
+
+        [Serializable]
+        public struct PolyphonicKeyPressureData
+        {
+            public int key;
+            public UnityEvent<byte> action;
+        }
+
+        [Serializable]
+        public struct ControlChangedData
+        {
+            public int controller;
+            public UnityEvent<byte> action;
+        }
+
+        [Serializable]
+        public struct ProgramChangedData
+        {
+            public int program;
+            public UnityEvent action;
+        }
+        #endregion
     }
 }
