834d7ad3b5b5256aafdf6200a5f346ca770a221c
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2022-09-29 12:10:36 +0200

    WIP

--- Assets/Shows/En Chiens/Runtime/Animations/SidesButtonsActivation.cs
+++ Assets/Shows/En Chiens/Runtime/Animations/SidesButtonsActivation.cs
@@ -1,11 +1,12 @@
+using Plml.Midi;
 using UnityEngine;
 
 namespace Plml.EnChiens.Animations
 {
     public class SidesButtonsActivation : MonoBehaviour
     {
-        public KeyCode side1Key;
-        public KeyCode side2Key;
+        public MidiNote side1Note;
+        public MidiNote side2Note;
 
         public float side1 => s1Value;
         public float side2 => s2Value;
@@ -20,14 +21,24 @@ namespace Plml.EnChiens.Animations
         private float s2Value = 0f;
         private float s2Vel = 0f;
 
+        private void Awake()
+        {
+            MidiInputListener mil = FindObjectOfType<MidiInputListener>();
+
+            mil.AddNoteOnListener(side1Note, SwitchOnSide1);
+            mil.AddNoteOffListener(side1Note, SwitchOffSide1);
+
+            mil.AddNoteOnListener(side2Note, SwitchOnSide2);
+            mil.AddNoteOffListener(side2Note, SwitchOffSide2);
+        }
+
+        public void SwitchOnSide1(byte _) => s1Target = 1f;
+        public void SwitchOffSide1(byte _) => s1Target = 0f;
+        public void SwitchOnSide2(byte _) => s2Target = 1f;
+        public void SwitchOffSide2(byte _) => s2Target = 0f;
+
         private void Update()
         {
-            if (Input.GetKeyDown(side1Key))
-                s1Target = s1Target == 0f ? 1f : 0f;
-
-            if (Input.GetKeyDown(side2Key))
-                s2Target = s2Target == 0f ? 1f : 0f;
-
             s1Value = Mathf.SmoothDamp(s1Value, s1Target, ref s1Vel, smoothTime);
             s2Value = Mathf.SmoothDamp(s2Value, s2Target, ref s2Vel, smoothTime);
         }
